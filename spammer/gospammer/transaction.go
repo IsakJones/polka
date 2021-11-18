54a321ac5ebcbdc1ad81f02b3f7eb9de728bbc70
IsakJones <isak.jones.98@gmail.com>
2021-11-18 22:35:49 +0100

    Simplified spammer

--- spammer/gospammer/transaction.go
+++ spammer/gospammer/transaction.go
@@ -15,7 +15,6 @@ const (
 	lo = 0
 	hi = 1000
 
-	channelCapacity = 100
 	contentType = "transaction/json"
 	timeout = 3 * time.Second
 )
@@ -46,9 +45,8 @@ var banks = []string{
 // close to simultaneous as possible.
 func TransactionSpammer(dest string, times int) {
 
+	// This splice will store the generated transactions
 	reqCollection := make([]bytes.Buffer, times)
-	chanCollection := make ([]chan *http.Response, times)
-	// respChannel := make(chan *http.Response, channelCapacity)
 
 	// Generate request payloads
 	rand.Seed(time.Now().UnixNano())
@@ -72,33 +70,21 @@ func TransactionSpammer(dest string, times int) {
 		}
 	}
 
-	// Send the number of requests
-	// start := time.Now()
+	
 	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	// Send requests
 	fmt.Printf("\nSending %d requests concurrently.\n", len(reqCollection))
 	for i := 0; i < times; i++ {
-		go SendTransaction(dest, &reqCollection[i], chanCollection[i], &wg)
-		go ProcessResponse(chanCollection[i], &wg)
+		go SendTransaction(dest, &reqCollection[i], &wg)
 	}
-
-	// Process the responses
-	// for i := 0; i < times; i++ {
-	// 	select {
-	// 	case resp := <-respChannel:
-	// 		ProcessResponse(resp)
-	// 	// Break if responses lag
-	// 	case <-time.After(timeout):
-	// 		fmt.Println("Waited response for too long.")
-	// 		break
-	// 	}
-	// }
-	// end := time.Now()
-	// fmt.Println(end.Sub(start))
+	
 }
 
 // SendTransaction sends a post request with transaction information
 // and prints the response's contents.
-func SendTransaction(dest string, payload *bytes.Buffer, respChannel chan<- *http.Response, wg *sync.WaitGroup) {
+func SendTransaction(dest string, payload *bytes.Buffer, wg *sync.WaitGroup) {
 
 	// Update waitgroup
 	wg.Add(1)
@@ -110,19 +96,6 @@ func SendTransaction(dest string, payload *bytes.Buffer, respChannel chan<- *htt
 		panic(err)
 	}
 
-	// Pass response in the channel
-	respChannel <- resp
-}
-
-// Prints the contents of responses and "handles" errors.
-func ProcessResponse(channel <-chan *http.Response, wg *sync.WaitGroup) {
-
-	// Update waitgroup
-	wg.Add(1)
-	defer wg.Done()
-
-	// Get response from channel
-	resp := <- channel
 	defer resp.Body.Close()
 	fmt.Println("Response status:", resp.Status)
 
@@ -134,7 +107,6 @@ func ProcessResponse(channel <-chan *http.Response, wg *sync.WaitGroup) {
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
-
 }
 
 // Returns a buffer fit for the POST request encoding
