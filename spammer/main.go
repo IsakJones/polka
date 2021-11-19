2555ff03f927ab933b3a1a42dec944629c779cc5
IsakJones <isak.jones.98@gmail.com>
2021-11-19 16:15:38 +0100

    updated spammer

--- spammer/main.go
+++ spammer/main.go
@@ -1,17 +1,33 @@
 package main
 
-import "github.com/IsakJones/polka/spammer/gospammer"
+import (
+	"log"
+	"os"
+	"strconv"
+	
+	"github.com/IsakJones/polka/spammer/gospammer"
+)
 
 const (
-	requestNumber   = 4000
+	requestNumber   = 9600
 	transactionDest = "http://localhost:8090/transaction"
 	// helloDest = "http://localhost:8090/hello"
 )
 
 func main() {
 
-	// gospammer.SayHello(helloDest)
-	// gospammer.SendTransaction(transactionDest)
+	// Check for right args
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("No transaction number provided.")
+	} else if len(args) > 2 {
+		log.Fatal("Too many arguments provided.")
+	}
 
-	gospammer.TransactionSpammer(transactionDest, requestNumber)
+	// Parse number of requests 
+	requestNumber, err := strconv.ParseInt(args[1], 10, 32)
+	if err != nil {
+		log.Fatal("Argument must be an integer.")
+	}
+	gospammer.TransactionSpammer(transactionDest, int(requestNumber))
 }
