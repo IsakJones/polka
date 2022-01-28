9e46559d423ceac6ba006fd209ba42a37417eedf
IsakJones <isak.jones.98@gmail.com>
2022-01-28 19:53:38 +0100

    changed to atomics

---
+++ generator/python_spammer/enums.py
@@ -0,0 +1,19 @@
+
+POLKA = "http://localhost:8090"
+
+# Views
+HELLO = "/hello"
+TRANSACTION = "/transaction"
+
+BANKS = [
+    "JP Morgan Chase",
+    "Bank of America",
+    "Wells Fargo",
+    "Citigroup",
+    "U.S. Bancorp",
+    "Truist Financial",
+    "PNC Financial Services Group",
+    "TD Group US",
+    "Bank of New York Mellon",
+    "Capital One Financial",
+]
